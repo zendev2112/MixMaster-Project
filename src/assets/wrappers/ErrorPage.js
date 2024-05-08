4c2aae42cc146c1c9cafdb1d91ee48d5136e5c40
zen <zenon.deviagge@gmail.com>
2024-05-08 18:05:33 -0300

    Style Error page

--- src/assets/wrappers/ErrorPage.js
+++ src/assets/wrappers/ErrorPage.js
@@ -7,8 +7,7 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
   img {
-    width: 90vw;
-    max-width: 600px;
+    max-width:600px;
     display: block;
     margin-bottom: 2rem;
     margin-top: -3rem;
@@ -16,12 +15,11 @@ const Wrapper = styled.div`
   h3 {
     margin-bottom: 0.5rem;
   }
-
   p {
     line-height: 1.5;
     margin-top: 0.5rem;
     margin-bottom: 1rem;
-    color: var(--grey-500);
+    color: var(--grey-500)
   }
   a {
     color: var(--primary-500);
