673f00a79ef0db501ecae86b30432b9c081f7407
zen <zenon.deviagge@gmail.com>
2024-05-08 16:57:44 -0300

    Wrap all pages below Navbar with the same styles

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,15 @@
-import { Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const HomeLayout = () => {
   return (
-    <div>
-      
-      <Outlet/>
-    </div>
+    <>
+      <Navbar />
+      {/* n React Router, the <Outlet/> component is a placeholder where child routes are rendered. It is typically used in layout components to render the child routes defined in the parent route component. */}
+      <section className="page">
+        <Outlet />
+      </section>
+    </>
   )
 }
 export default HomeLayout
