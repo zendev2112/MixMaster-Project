ff8edae013eb11e5b38033349181c49f0e26acc2
zen <zenon.deviagge@gmail.com>
2024-05-09 16:29:41 -0300

    Fetch drinks array from drinks API. Log it in the console

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,19 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from "react-router-dom";
+import axios from 'axios'
+
+
+const cocktailSearchUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 export const loader = async () => {
-  return 'something'
+  const searchTerm = 'margarita'
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  return {drinks:response.data.drinks, searchTerm}
 }
 
 const Landing = () => {
-  const data =  useLoaderData();
-  console.log(data);
+  const {drinks, searchTerm} =  useLoaderData();
+  console.log(drinks);
   return (
     <h1>Landing</h1>
   )
