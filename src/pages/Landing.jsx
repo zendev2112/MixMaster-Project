2c275dd51a4aeccc46e3ae2e50076f03ffcbd5bc
zen <zenon.deviagge@gmail.com>
2024-05-10 19:18:29 -0300

    Return CocktailList component from Landing component. Pass `drinks` prop down to CocktailList component. Destructure `drinks` properties and pass it down as attributes of `CocktailCard`. Wrap `CocktailList` with styles

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,21 +1,24 @@
 import { useLoaderData } from "react-router-dom";
 import axios from 'axios'
+import CocktailList from '../components/CocktailList'
 
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 export const loader = async () => {
-  const searchTerm = 'margarita'
+  const searchTerm = ''
   const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
   return {drinks:response.data.drinks, searchTerm}
 }
 
 const Landing = () => {
   const {drinks, searchTerm} =  useLoaderData();
-  console.log(drinks);
+  
   return (
-    <h1>Landing</h1>
+    <>
+    <CocktailList drinks={drinks}/>
+    </>
   )
 }
 export default Landing
