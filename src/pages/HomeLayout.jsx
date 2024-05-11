766866ca7a8001f1111a23fb6514adfc12e385d9
zen <zenon.deviagge@gmail.com>
2024-05-10 21:11:59 -0300

    Import useNavigation hook from react-router-dom in HomeLayout component. If the hook state is `loading`, display the loading circle

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,13 +1,18 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
 const HomeLayout = () => {
+  const navigation = useNavigation()
+  console.log(navigation)
+  const isPageLoading = navigation.state === 'loading';
+  const value = 'some value'
+
   return (
     <>
       <Navbar />
       {/* n React Router, the <Outlet/> component is a placeholder where child routes are rendered. It is typically used in layout components to render the child routes defined in the parent route component. */}
       <section className="page">
-        <Outlet />
+        {isPageLoading ? <div className="loading"></div> : <Outlet context={{value}} />}
       </section>
     </>
   )
