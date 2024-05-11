57b13432eb4769493d0314e739749c7d85d8c52a
zen <zenon.deviagge@gmail.com>
2024-05-10 21:40:43 -0300

    Define `loader` function. Get the id from the params and `data` with get method. Destructure id and data with useLoaderData. Export `loader` function as `singleCocktailLoader` in the router on App component

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,4 +1,17 @@
+import {useLoaderData, Link} from 'react-router-dom'; 
+import axios from 'axios';
+import Wrapper from '../assets/wrappers/CocktailPage';
+const singleCocktailUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+export const loader = async ({params}) => {
+  const {id} = params
+  const {data} = await axios.get(`${singleCocktailUrl}${id}`);
+  return {id, data}
+}
+
 const Cocktail = () => {
+  const {id, data} = useLoaderData();
   return (
     <h1>Cocktail</h1>
   )
