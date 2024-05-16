4b44691197bd6dad64bafa8630d2f0890e5ef39e
zen <zenon.deviagge@gmail.com>
2024-05-16 16:02:08 -0300

    Create a new URL object using the URL constructor on the loader function of the Landing page. Store the query search params on a variable. Pass it down as a prop (searchTerm) of the SearchForm component. Setup searchTerm as a default value on the SearchForm component input

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,15 @@
 import { useLoaderData } from 'react-router-dom'
 import axios from 'axios'
-import CocktailList from '../components/CocktailList';
+import CocktailList from '../components/CocktailList'
 import SearchForm from '../components/SearchForm'
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 
-export const loader = async () => {
-  const searchTerm = ''
+export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+
+  const searchTerm = url.searchParams.get('search') || ''
   const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
   return { drinks: response.data.drinks, searchTerm }
 }
@@ -17,7 +19,7 @@ const Landing = () => {
 
   return (
     <>
-      <SearchForm />
+      <SearchForm searchTerm={searchTerm}/>
       <CocktailList drinks={drinks} />
     </>
   )
